824afb12ed627ca49987bdd5aef2dc18f81e609c
Kanikashrivastav <kanika17@navgurukul.org>
2018-10-12 11:34:24 +0530

    Updates

---
+++ precache-manifest.98690bfc34aa20c1a50a55f63bb95885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b979d1b03af982ede93a",
+    "url": "/emoji-search-react-filter/static/js/runtime~main.b979d1b0.js"
+  },
+  {
+    "revision": "1cb13c2ea5e0aee687ac",
+    "url": "/emoji-search-react-filter/static/js/main.1cb13c2e.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/emoji-search-react-filter/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "1cb13c2ea5e0aee687ac",
+    "url": "/emoji-search-react-filter/static/css/main.4b51fb6e.chunk.css"
+  },
+  {
+    "revision": "a9dd4faa5f9f7e5ed729e314d9b79290",
+    "url": "/emoji-search-react-filter/index.html"
+  }
+];
